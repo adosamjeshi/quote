711a8a4416bd81fadf7a24a3839baefb62688931
Adan Muktar <adosamjeshi@gmail.com>
2020-04-07 23:16:49 +0300

    changes in ip

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,11 +11,7 @@ export class QuoteComponent implements OnInit {
   quotes = [
 
     new Quote(1, "Indira Gandhi", "You cannot shake hands with a clenched fist", new Date(2016.11, 15)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+
   ];
 
    
