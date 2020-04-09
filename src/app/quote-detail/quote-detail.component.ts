8e30957d5b67755e8380744bf4b0065042451f90
Adan Muktar <adosamjeshi@gmail.com>
2020-04-09 14:36:51 +0300

    changes

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -6,7 +6,7 @@ import { Quote } from '../quote'
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
-export class QuoteDetailsComponent implements OnInit {
+export class QuoteDetailComponent implements OnInit {
   @Input() quote:Quote;
   @Output() isComplete= new EventEmitter<boolean>();
   quoteDelete(complete:boolean){
